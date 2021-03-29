23656dbd9229209373c327362aa05625cb516566
suryankdixit <suryankdixit@gmail.com>
2021-03-29 23:23:21 +0530

    Splitting of leaf node implemented

--- table.cpp
+++ table.cpp
@@ -64,6 +64,7 @@ Table* Table :: db_open(const char* filename)
         // page 0 = leaf node;
         void* root = get_page(pager,0);
         initialize_leaf_node(root);
+        set_node_root(root, true);
     }
     return table;
 }
@@ -239,88 +240,3 @@ void Row :: deserialize_data(void* source,Row* destination){
 void Row :: print_row(Row* row){
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
-
-
-void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value) {
-  void* node = get_page(cursor->table->get_pager(), cursor->page_num);
-
-//   cout<<value->id<<" "<<value->email<<" "<<value->username<<endl;
-
-  uint32_t num_cells = *leaf_node_num_cells(node);
-  if (num_cells >= LEAF_NODE_MAX_CELLS) {
-    // Node full
-    printf("Need to implement splitting a leaf node.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  if (cursor->cell_num < num_cells) {
-    // Make room for new cell
-    for (uint32_t i = num_cells; i > cursor->cell_num; i--) {
-      memcpy(leaf_node_cell(node, i), leaf_node_cell(node, i - 1),LEAF_NODE_CELL_SIZE);
-    }
-  }
-
-  *(leaf_node_num_cells(node)) += 1;
-  *(leaf_node_key(node, cursor->cell_num)) = key;
-  value->serialize_data(value, leaf_node_value(node, cursor->cell_num));
-}
-
-
-NodeType get_node_type(void* node) {
-  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
-  return (NodeType)value;
-}
-
-void set_node_type(void* node, NodeType type) {
-  uint8_t value = type;
-  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
-}
-
-
-Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key) {
-  void* node = get_page(table->get_pager(), page_num);
-  uint32_t num_cells = *leaf_node_num_cells(node);
-
-  // Cursor* cursor = malloc(sizeof(Cursor));
-  Cursor* cursor = new Cursor;
-  cursor->table = table;
-  cursor->page_num = page_num;
-
-  // Binary search
-  uint32_t min_index = 0;
-  uint32_t one_past_max_index = num_cells;
-  while (one_past_max_index != min_index) {
-    uint32_t index = (min_index + one_past_max_index) / 2;
-    uint32_t key_at_index = *leaf_node_key(node, index);
-    if (key == key_at_index) {
-      cursor->cell_num = index;
-      return cursor;
-    }
-    if (key < key_at_index) {
-      one_past_max_index = index;
-    } else {
-      min_index = index + 1;
-    }
-  }
-
-  cursor->cell_num = min_index;
-  return cursor;
-}
-
-
-/*
-Return the position of the given key.
-If the key is not present, return the position where it should be inserted
-*/
-Cursor* table_find(Table* table, uint32_t key) {
-  uint32_t root_page_num = table->get_root();
-  void* root_node = get_page(table->get_pager(), root_page_num);
-
-  if (get_node_type(root_node) == NODE_LEAF) {
-    return leaf_node_find(table, root_page_num, key);
-  } else {
-    printf("Need to implement searching an internal node\n");
-    exit(EXIT_FAILURE);
-  }
-}
-
